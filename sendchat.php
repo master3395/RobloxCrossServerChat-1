b7e164354707404fe35d210a7930ac659534b01f
Argelius <Comrogue@users.noreply.github.com>
2015-09-13 14:53:39 -0400

    Update sendchat.php
    Moved second SQL query

--- sendchat.php
+++ sendchat.php
@@ -10,14 +10,16 @@ if(mysqli_connect_errno()){
 echo("Failed to connect to database. This is a server-side issue.");
 }
 elseif($gameid!=NULL and $username!=NULL and $message!=NULL){
+if(!stristr($message,"</head>") and !stristr($message,"<username>") and !stristr($message,"</username>") and !stristr($message,"</message>") and !stristr($message,"<message>")){
 $data = mysqli_query($con,"INSERT INTO Games (gameid,username,message) VALUES ('$gameid','$username','$message')");
+}
 if(!$data){
 echo("Error sending data.");
 }
 else{
 echo("Data received.");
-}
 mysqli_query($con,"DELETE FROM Games ORDER BY messageid LIMIT 1");
+}
 }
 else{
 echo("Check to make sure you are sending all required POST variables. We received: ".$gameid.$username.$message);
